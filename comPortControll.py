438b76c6bfdd116e8ea46a3234fd5347ade6e13c
IJ Chang <ij.chang@microchip.com>
2020-12-02 10:59:18 +0800

    Modify comPortControll.py

--- comPortControll.py
+++ comPortControll.py
@@ -1,5 +1,20 @@
 import serial
-#test
+
+def ExtAdvProc(evtLenStr):
+    evtData = ser.read(int(evtLenStr, 16)) #read the data of this le meta event
+    print(evtData)
+    numOfRpts = hex(evtData[0])
+    evtTypes = hex(evtData[1]) + hex(evtData[2])
+    addrTypes = hex(evtData[3])
+    addr = hex(evtData[4]) + hex(evtData[5]) + hex(evtData[6]) + hex(evtData[7]) + hex(evtData[8]) + hex(evtData[9])
+
+    print('numOfRpts = ', numOfRpts)
+    print('evtTypes = ', evtTypes)
+    print('addr = ', addr)
+    
+    evtAll = '043E' + evtLenStr + evtData.hex() #compose the full hci event
+    print(evtAll) #print the result
+
 ser = serial.Serial('/dev/ttyS4', baudrate=115200) #open serial port
 print(ser.name) #check which port was really used
 
@@ -15,12 +30,15 @@ while 1:
         continue
 
     evtLen = ser.read(1)#get the len of the data of this hci le meta event
+    evtCode = ser.read(1)#get the subevent code
     evtLenStr = evtLen.hex()
+    evtCodeStr = evtCode.hex()
     #print(type(evtLenStr))
     #print(evtLenStr)
-    evtData = ser.read(int(evtLenStr, 16)) #read the data of this le meta event
-    evtAll = '043E' + evtLenStr + evtData.hex() #compose the full hci event
-    print(evtAll) #print the result
+    if evtCodeStr == '0d':
+        print('ExtAdvRpt\n')
+        ExtAdvProc(evtLenStr)
+    
     stop = input('please enter 1 to stop')
     if int(stop) == 1:
         break
